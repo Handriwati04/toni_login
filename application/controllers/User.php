99274fed69fafaee12e78925bdd4e8ac763ec6fd
wahlulwathoni10 <m.wahlulwathoni03@gmail.com>
2021-10-04 15:31:42 +0700

    add commit

---
+++ application/controllers/User.php
@@ -0,0 +1,53 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class User extends CI_Controller
+{
+
+    public function index()
+    {
+        $data['list_nama2'] = $this->db->get('list_nama2')->result();
+        $this->load->view('user/index', $data);
+    }
+    public function tambah_hal()
+    {
+        $this->load->view('user/tambahhalmhs');
+    }
+    public function tambah_edit()
+    {
+        $this->load->view('user/tambahmhs');
+    }
+    public function create()
+    {
+        if ($this->input->server('REQUEST_METHOD') == 'POST') {
+            $data = $this->input->post();
+
+            $this->db->insert('list_nama2', $data);
+            redirect('user');
+        } else {
+            $this->load->view('user/create');
+        }
+    }
+    public function update($id)
+    {
+        if ($this->input->server('REQUEST_METHOD') == 'POST') {
+            $data = $this->input->post();
+
+
+            $this->db->where('id', $id);
+            $this->db->update('list_nama2', $data);
+
+            redirect('user'); //halaman
+        } else {
+            $data['item'] = $this->db->get_where('list_nama2', ['id' => $id])->row();
+            $this->load->view('user/update', $data);
+        }
+    }
+    public function delete($id)
+    {
+        $this->db->where('id', $id);
+        $this->db->delete('list_nama2');
+
+        redirect('user');
+    }
+}
